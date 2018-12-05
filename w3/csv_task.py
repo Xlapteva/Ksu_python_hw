402f24572bae828e338e45190881c08a8a93cb91
Ksenia Laptevav <x.lapteva@gmail.com>
2018-12-05 13:03:37 +0300

    Добавила домашку третьей недели

---
+++ w3/csv_task.py
@@ -0,0 +1,13 @@
+import csv
+
+user_list = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'}, 
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+    ]
+with open('csv_domashka.csv', 'w', encoding = 'utf-8') as f:
+    fields = ['name', 'age', 'job']
+    writer = csv.DictWriter(f, fields, delimiter = ';')
+    writer.writeheader()
+    for user in user_list:
+        writer.writerow(user)
